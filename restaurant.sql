ab6626a050151baa81ffe44f0a7133bbc3954a73
akalka-indumina <akalka234@gmail.com>
2025-11-24 23:08:12 +0530

    Add files via upload

--- restaurant.sql
+++ restaurant.sql
@@ -147,3 +147,25 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Sample users
+INSERT INTO `users` (`username`, `password`, `role`) VALUES
+('admin1', 'admin1', 'ADMIN'),
+('admin2', 'admin2', 'ADMIN'),
+('cashier1', 'cashier1', 'CASHIER'),
+('cashier2', 'cashier2', 'CASHIER');
+
